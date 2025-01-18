90c8e1d9a993624d5231c386bae592698bb7a49e
AllanC12 <allandevfront@gmail.com>
2025-01-18 16:22:26 -0300

    feat - melhorando filtro de busca de cliente

--- ViewModels/ClientViewModel.cs
+++ ViewModels/ClientViewModel.cs
@@ -3,6 +3,8 @@
     public class ClientViewModel
     {
         public string Name { get; set; }
+
+        public string LastName { get; set; }
         public DateOnly Birthday { get; set; }
         public string Favorite_Service { get; set; }
         public string Observation { get; set; }
